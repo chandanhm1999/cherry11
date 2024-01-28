81b9b2cd9820d49f0a9dbda0ff91ef4be2a5fb28
chandan hm <chandanhm388@gmail.com>
2024-01-28 12:09:54 +0530

    cherry namkarna temp

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { createRoot } from 'react-dom/client';
+import App from "./App";
+
+const root = document.getElementById('root');
+const rootContainer = createRoot(root);
+rootContainer.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
